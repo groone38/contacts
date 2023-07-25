470d359541bc5b5089bb31652e5e775b9ea3e098
groone38 <groone38@yandex.ru>
2023-07-25 17:28:59 +0300

    create base routing

--- src/pages/Layout/ui/Layout.tsx
+++ src/pages/Layout/ui/Layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import classes from "./Layout.module.scss";
 
 const Layout = () => {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <header>Header</header>
       <main>
         <Outlet />
